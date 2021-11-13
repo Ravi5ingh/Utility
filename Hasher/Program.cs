38f320d180e2b2a40c725d8bdfd9de8abf646fb4
Ravi Singh <ravisingh1203@hotmail.com>
2021-11-13 22:29:50 +0000

    add Hasher command line utility

---
+++ Hasher/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TextCopy;
+
+namespace Hasher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("At least one command line arg is required. (eg. Haser.exe abc) will put the hash for 'abc' in your copy buffer");
+                Environment.Exit(1);
+            }
+            ClipboardService.SetText(BCrypt.Net.BCrypt.HashPassword(args[0], BCrypt.Net.BCrypt.GenerateSalt(12)));
+            Console.WriteLine($"Hash for {args[0]} is now in copy buffer");
+        }
+    }
+}
